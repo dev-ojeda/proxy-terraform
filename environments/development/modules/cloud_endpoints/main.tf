2e6373f69e4c1eb3836a3d782d179aba13a1b5d9
dev-ojeda <neo1sr3@gmail.com>
2025-08-31 05:23:26 -0400

    Update

--- environments/development/modules/cloud_endpoints/main.tf
+++ environments/development/modules/cloud_endpoints/main.tf
@@ -7,7 +7,10 @@ terraform {
   }
 }
 resource "google_endpoints_service" "openapi_service" {
-  service_name   = var.service_name
-  project        = var.project_id
-  openapi_config = file("${path.module}/${var.openapi_path}")
+  service_name   = var.service_tags.service_app_proxy
+  project        = var.project_tags.project_id
+  openapi_config = templatefile("${path.module}/openapi-run.yaml", {
+    HOST     = var.service_tags.service_host_name
+    ENDPOINT = "https://${var.service_tags.service_url_name}-${var.project_tags.project_number}.${var.project_tags.region}.run.app"
+  })
 }
